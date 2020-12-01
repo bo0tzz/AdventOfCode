98bb222a3d8e7518acf5b956d7a9408a8a6adfdc
Boet de Willigen <boet.dewilligen@securew2.com>
2020-12-01 12:12:31 +0100

    Run all day modules asynchronously

--- 2020/lib/advent_of_code.ex
+++ 2020/lib/advent_of_code.ex
@@ -17,11 +17,35 @@ defmodule AdventOfCode do
 
   """
   def main do
-    for module <- @aoc_modules do
-      read_input(module)
-      |> module.test
-      |> IO.puts
+    children = Enum.map(@aoc_modules, &run_module_async/1)
+    receive_results(Enum.count(children))
+  end
+
+  def receive_results(count) do
+    if count == 0 do
+      :done
+    else
+      receive do
+        {module, result} -> log_result(module, result)
+      end
+      receive_results(count - 1)
     end
+  end
+
+  def log_result(module, result) do
+    IO.inspect "Got result from module " <> Atom.to_string(module) <> ": [" <> Kernel.inspect(result) <> "]"
+  end
+
+  def run_module_async(module) do
+    current = self()
+    spawn (fn -> run_module(module, current) end)
+  end
+
+  def run_module(module, parent) do
+    result = read_input(module)
+    |> module.test
+
+    send(parent, {module, result})
   end
 
   def read_input(module) do
