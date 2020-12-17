b9668c0931a24d393e616a24caff2999cdd1252e
bo0tzz <btdewilligen@gmail.com>
2020-12-17 12:26:01 +0100

    Day17.2 solution

---
+++ 2020/lib/days/day17_2.ex
@@ -0,0 +1,81 @@
+defmodule Day17_2 do
+  def test(input) do
+    parse_initial_state(input)
+    |> cycle_count(6)
+    |> Map.values()
+    |> Enum.filter(&match?(:active, &1))
+    |> Enum.count()
+  end
+
+  def cycle_count(state, 0), do: state
+
+  def cycle_count(state, n) do
+    IO.puts("running cycle #{n}")
+    cycle(state) |> cycle_count(n - 1)
+  end
+
+  def cycle(state) do
+    Map.keys(state)
+    |> all_neighbours()
+    |> Enum.reduce(%{}, fn cube, new_state ->
+      active_neighbours =
+        neighbours(cube)
+        |> Enum.map(&get_cube_state(&1, state))
+        |> Enum.count(&match?(:active, &1))
+
+      cube_state =
+        case active_neighbours do
+          3 -> :active
+          2 -> get_cube_state(cube, state)
+          _ -> :inactive
+        end
+
+      Map.put(new_state, cube, cube_state)
+    end)
+  end
+
+  def all_neighbours(locations) do
+    for l <- locations do
+      neighbours_keep_original(l)
+    end
+    |> List.flatten()
+    |> Enum.uniq()
+  end
+
+  def get_cube_state(location, state) do
+    Map.get(state, location, :inactive)
+  end
+
+  def neighbours({_x, _y, _z, _w} = n) do
+    neighbours_keep_original(n)
+    |> List.delete(n)
+  end
+
+  def neighbours(n) do
+    (n - 1)..(n + 1)
+  end
+
+  def neighbours_keep_original({x, y, z, w}) do
+    for x <- neighbours(x), y <- neighbours(y), z <- neighbours(z), w <- neighbours(w) do
+      {x, y, z, w}
+    end
+  end
+
+  def parse_initial_state(state) do
+    Util.lines(state)
+    |> Enum.with_index()
+    |> Enum.map(fn {line, y} ->
+      String.trim(line)
+      |> String.codepoints()
+      |> Enum.with_index()
+      |> Enum.map(fn {c, x} ->
+        case c do
+          "." -> {{x, y, 0, 0}, :inactive}
+          "#" -> {{x, y, 0, 0}, :active}
+        end
+      end)
+    end)
+    |> List.flatten()
+    |> Map.new()
+  end
+end
